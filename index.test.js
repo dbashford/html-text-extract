b2bef8f8e4d57fbc453b625c07d2c94d7d44f168
Matthew Tylee Atkinson <matatk@gmail.com>
2018-01-18 14:18:30 +0000

    Re-organise tests for consistency; slightly simplify one

--- index.test.js
+++ index.test.js
@@ -10,22 +10,22 @@ describe('simple text extraction', () => {
 		expect(hte('<p>Hello <span>there</span>, world!</p>'))
 			.toEqual('Hello there, world!')
 	})
-})
 
-describe('spacing with text', () => {
-	it('adds a space between heading and paragraph', () => {
-		expect(hte('<h1>Welcome</h1><p>Hello <span>there</span>, world!</p>'))
-			.toEqual('Welcome Hello there, world!')
-	})
+	describe('spacing with text', () => {
+		it('adds a space between heading and paragraph', () => {
+			expect(hte('<h1>Welcome</h1><p>Hello, world!</p>'))
+				.toEqual('Welcome Hello, world!')
+		})
 
-	it('adds a space between two headings', () => {
-		expect(hte('<h1>moo1</h1><h2>moo2</h2>'))
-			.toEqual('moo1 moo2')
-	})
+		it('adds a space between two headings', () => {
+			expect(hte('<h1>moo1</h1><h2>moo2</h2>'))
+				.toEqual('moo1 moo2')
+		})
 
-	it('adds a space between two headings and a paragraph', () => {
-		expect(hte('<h1>moo1</h1><h2>moo2</h2><p>moop</p>'))
-			.toEqual('moo1 moo2 moop')
+		it('adds a space between two headings and a paragraph', () => {
+			expect(hte('<h1>moo1</h1><h2>moo2</h2><p>moop</p>'))
+				.toEqual('moo1 moo2 moop')
+		})
 	})
 })
 
