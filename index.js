50e2c59c14026a6af28d64ee37debccf6a1c703b
Matthew Tylee Atkinson <matatk@gmail.com>
2018-01-18 13:13:05 +0000

    Generalise the text-extraction

--- index.js
+++ index.js
@@ -2,7 +2,25 @@
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
+function walkTextCore(node, dom, text) {
+	let newText = ''
+	for (const child of node.childNodes) {
+		if (child.nodeType === dom.window.Node.ELEMENT_NODE) {
+			newText = newText + walkTextCore(child, dom, text)
+		} else if (child.nodeType === dom.window.Node.TEXT_NODE) {
+			newText = newText + child.textContent
+		}
+	}
+	return text + newText
+}
+
+function walkText(dom) {
+	const body = dom.window.document.body
+	const text = walkTextCore(body, dom, '')
+	return text
+}
+
 module.exports = function(html) {
 	const dom = new JSDOM(html)
-	return dom.window.document.querySelector('p').textContent
+	return walkText(dom)
 }
--- index.test.js
+++ index.test.js
@@ -1,6 +1,11 @@
 'use strict'
 const hte = require('./')
 
-it('extracts text', () => {
+it('extracts text from a single paragraph', () => {
 	expect(hte('<p>Hello, world!</p>')).toEqual('Hello, world!')
+})
+
+it('extracts text from a single paragraph with embedded span', () => {
+	expect(hte('<p>Hello <span>there</span>, world!</p>'))
+		.toEqual('Hello there, world!')
 })
