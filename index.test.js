2564265aebbaffd703de1a6a340b208065e54412
Matthew Tylee Atkinson <matatk@gmail.com>
2018-01-18 14:02:36 +0000

    Re-organise tests for clarity

--- index.test.js
+++ index.test.js
@@ -1,31 +1,37 @@
 'use strict'
 const hte = require('./')
 
-it('extracts text from a single paragraph', () => {
-	expect(hte('<p>Hello, world!</p>')).toEqual('Hello, world!')
+describe('simple text extraction', () => {
+	it('extracts text from a single paragraph', () => {
+		expect(hte('<p>Hello, world!</p>')).toEqual('Hello, world!')
+	})
+
+	it('extracts text from a single paragraph with embedded span', () => {
+		expect(hte('<p>Hello <span>there</span>, world!</p>'))
+			.toEqual('Hello there, world!')
+	})
 })
 
-it('extracts text from a single paragraph with embedded span', () => {
-	expect(hte('<p>Hello <span>there</span>, world!</p>'))
-		.toEqual('Hello there, world!')
+describe('spacing with text', () => {
+	it('adds a space between heading and paragraph', () => {
+		expect(hte('<h1>Welcome</h1><p>Hello <span>there</span>, world!</p>'))
+			.toEqual('Welcome Hello there, world!')
+	})
+
+	it('adds a space between two headings', () => {
+		expect(hte('<h1>moo1</h1><h2>moo2</h2>'))
+			.toEqual('moo1 moo2')
+	})
+
+	it('adds a space between two headings and a paragraph', () => {
+		expect(hte('<h1>moo1</h1><h2>moo2</h2><p>moop</p>'))
+			.toEqual('moo1 moo2 moop')
+	})
 })
 
-it('adds a space between heading and paragraph', () => {
-	expect(hte('<h1>Welcome</h1><p>Hello <span>there</span>, world!</p>'))
-		.toEqual('Welcome Hello there, world!')
-})
-
-it('adds a space between two headings', () => {
-	expect(hte('<h1>moo1</h1><h2>moo2</h2>'))
-		.toEqual('moo1 moo2')
-})
-
-it('adds a space between two headings and a paragraph', () => {
-	expect(hte('<h1>moo1</h1><h2>moo2</h2><p>moop</p>'))
-		.toEqual('moo1 moo2 moop')
-})
-
-it('extracts alt text from images', () => {
-	expect(hte('<img alt="description" src="...">'))
-		.toEqual('description')
+describe('alt text', () => {
+	it('extracts alt text from images', () => {
+		expect(hte('<img alt="description" src="...">'))
+			.toEqual('description')
+	})
 })
